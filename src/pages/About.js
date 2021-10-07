1629b33c1c508dbbdd3ef6a3ff431e8323a14632
aaahana <e10991aaahana@dpsrkp.net>
2021-10-07 19:44:51 +0530

    add image to about

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ import React from 'react'
 import AboutCard from '../components/AboutCard'
 import Navbar from '../components/Navbar'
 import  "../scss/components/aboutcard.scss"
+import aaahanaImg from "../assets/about/aaahana.png"
 
 const About = () => {
     return (
@@ -14,7 +15,7 @@ const About = () => {
             intro={"Hi! I am Darsh Sikka from New Delhi. I am a passionate mathematician. I am a web develpor at exun clan. I like coding in MERN stack, it is a stress buster for me and I want to utilize it for the good of others. I am also the founder of Rev Mate your mate for revision in exams"}
             />
             <AboutCard 
-            image={""}
+            image={aaahanaImg}
             name={"Aaahana Mehrotra"}
             intro={"Hi, I am Aaahana Mehrotra, a student of class 9 at DPS RK Puram with interests in computer science, physics and mathematics. I am a web development enthusiast. I am a keen learner and observer and hope to use my skills in order to improve the society"}
             />
